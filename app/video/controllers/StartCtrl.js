0460398789b56098e6c67c6c9e383c71c7a410f1
Christian Brel <ch.brel@gmail.com>
2016-04-13 10:24:13 +0200

    refs INTERACT-13 : First version with good behaviour. Code need to be review !

--- app/video/controllers/StartCtrl.js
+++ app/video/controllers/StartCtrl.js
@@ -8,7 +8,7 @@
  * Controller of the PulseTotemVideo
  */
 angular.module('PulseTotemVideo')
-  .controller('PulseTotemVideo.StartCtrl', ['$rootScope', '$scope', 'CONSTANTS', function ($rootScope, $scope, CONSTANTS) {
+  .controller('PulseTotemVideo.StartCtrl', ['$rootScope', '$scope', '$timeout', 'CONSTANTS', 'Upload', function ($rootScope, $scope, $timeout, CONSTANTS, Upload) {
     $rootScope.activeMenu = 'video';
     $rootScope.activeNavbar = '';
 
@@ -16,12 +16,114 @@ angular.module('PulseTotemVideo')
     var stopRecording = document.getElementById('stop-recording');
     var cameraPreview = document.getElementById('camera-preview');
 
-    var audio = document.querySelector('audio');
+    cameraPreview.muted = true;
 
-    var isFirefox = !!navigator.mozGetUserMedia;
+    function successCallback(stream) {
+      // RecordRTC usage goes here
 
-    var recordAudio, recordVideo;
-    startRecording.onclick = function() {
+      var optionsVideo = {
+        type: 'video',
+        frameInterval: 20 // minimum time between pushing frames to Whammy (in milliseconds)
+      };
+      $scope.recordRTCVideo = RecordRTC(stream, optionsVideo);
+
+      var optionsAudio = {
+        type: 'audio',
+        bufferSize: 16384
+      };
+      $scope.recordRTCAudio = RecordRTC(stream, optionsAudio);
+
+      cameraPreview.src = window.URL.createObjectURL(stream);
+      cameraPreview.play();
+
+      startRecording.disabled = false;
+    }
+
+    function errorCallback(error) {
+      consle.log(error);
+    }
+
+    var mediaConstraints = {
+      video: true,
+      audio: true
+    };
+
+    navigator.getUserMedia(mediaConstraints, successCallback, errorCallback);
+
+    $scope.startRecording = function() {
+      startRecording.disabled = true;
+
+      $scope.recordRTCVideo.startRecording();
+      $scope.recordRTCAudio.startRecording();
+
+      stopRecording.disabled = false;
+    };
+
+    $scope.audioDataURL = null;
+    $scope.videoDataURL = null;
+
+    $scope.audioBlob = false;
+    $scope.videoBlob = false;
+
+    $scope.formD = new FormData();
+    $scope.filename = getRandomString();
+    $scope.formD['filename'] = $scope.filename;
+    console.log($scope.formD);
+
+    $scope.stopRecording = function() {
+      startRecording.disabled = false;
+      stopRecording.disabled = true;
+
+      $scope.recordRTCAudio.stopRecording(function(audioURL) {
+        console.log("stop audio");
+
+        var audioBlob = $scope.recordRTCAudio.getBlob();
+
+        $scope.formD['audio-blob'] = audioBlob;
+        console.log($scope.formD);
+        $scope.audioBlob = true;
+
+        $scope.afterStopRecording();
+      });
+
+      $scope.recordRTCVideo.stopRecording(function(videoURL) {
+        console.log("stop video");
+
+        var videoBlob = $scope.recordRTCVideo.getBlob();
+
+        $scope.formD['video-blob'] = videoBlob;
+        console.log($scope.formD);
+        $scope.videoBlob = true;
+
+        $scope.afterStopRecording();
+      });
+    };
+
+    $scope.afterStopRecording = function() {
+      /*$scope.recordRTCAudio.getDataURL(function(audioDataURL) {
+        $scope.audioDataURL = audioDataURL;
+        console.log("audio");
+        $scope.postFiles();
+      });
+
+      $scope.recordRTCVideo.getDataURL(function(videoDataURL) {
+        $scope.videoDataURL = videoDataURL;
+        console.log("video");
+        $scope.postFiles();
+      });*/
+
+      if($scope.audioBlob && $scope.videoBlob) {
+        $scope.postFiles();
+      }
+
+    };
+
+    //var audio = document.querySelector('audio');
+
+    var isFirefox = false;
+
+    /*var recordAudio, recordVideo;
+    $scope.startRecording = function() {
       startRecording.disabled = true;
       navigator.getUserMedia({
         audio: true,
@@ -53,7 +155,7 @@ angular.module('PulseTotemVideo')
     };
 
 
-    stopRecording.onclick = function() {
+    $scope.stopRecording = function() {
       startRecording.disabled = false;
       stopRecording.disabled = true;
 
@@ -75,45 +177,110 @@ angular.module('PulseTotemVideo')
           } else postFiles(audioDataURL);
         });
       }
-    };
+    };*/
 
-    var fileName;
 
-    function postFiles(audioDataURL, videoDataURL) {
-      fileName = getRandomString();
-      var files = { };
+    $scope.postFiles = function() {
+      console.log("postFiles");
 
-      files.audio = {
-        name: fileName + (isFirefox ? '.webm' : '.wav'),
-        type: isFirefox ? 'video/webm' : 'audio/wav',
-        contents: audioDataURL
-      };
+        cameraPreview.src = '';
+        //cameraPreview.poster = '/ajax-loader.gif';
 
-      if (!isFirefox) {
-        files.video = {
-          name: fileName + '.webm',
-          type: 'video/webm',
-          contents: videoDataURL
-        };
-      }
+      console.log($scope.formD);
 
-      files.isFirefox = isFirefox;
+        Upload.upload({
+          url: CONSTANTS.liveCommunityServiceUrl + CONSTANTS.liveCommunityUploadsPath,
+          headers: {},
+          data: {
+            file : $scope.formD
+          }
+        }).then(function (response) {
+          $timeout(function () {
+            console.log(response);
+          });
+        }, function (response) {
 
-      cameraPreview.src = '';
-      cameraPreview.poster = '/ajax-loader.gif';
+          /*if (response.status > 0) {
+           $scope.errorMsg = response.status + ': ' + response.data;
+           }*/
 
-      xhr(CONSTANTS.liveCommunityServiceUrl + CONSTANTS.liveCommunityUploadsPath, JSON.stringify(files), function(_fileName) {
-        var href = location.href.substr(0, location.href.lastIndexOf('/') + 1);
-        cameraPreview.src = href + 'uploads/' + _fileName;
-        cameraPreview.play();
+          //TODO : Display message to User
 
-        var h2 = document.createElement('h2');
-        h2.innerHTML = '<a href="' + cameraPreview.src + '">' + cameraPreview.src + '</a>';
-        document.body.appendChild(h2);
-      });
+        }, function (evt) {
+          /*$scope.progress =
+           Math.min(100, parseInt(100.0 * evt.loaded / evt.total));*/
+          //Nothing to do ?
+        });
     }
 
-    function xhr(url, data, callback) {
+    /*var fileName;
+
+    $scope.postFiles = function() {
+      console.log("postFiles");
+      if($scope.audioDataURL != null && $scope.videoDataURL != null) {
+        var audioDataURL = $scope.audioDataURL;
+        var videoDataURL = $scope.videoDataURL;
+
+
+        fileName = getRandomString();
+        var files = {};
+
+        files.audio = {
+          name: fileName + (isFirefox ? '.webm' : '.wav'),
+          type: isFirefox ? 'video/webm' : 'audio/wav',
+          contents: audioDataURL
+        };
+
+        if (!isFirefox) {
+          files.video = {
+            name: fileName + '.webm',
+            type: 'video/webm',
+            contents: videoDataURL
+          };
+        }
+
+        files.isFirefox = isFirefox;
+
+        cameraPreview.src = '';
+        //cameraPreview.poster = '/ajax-loader.gif';
+
+        Upload.upload({
+          url: CONSTANTS.liveCommunityServiceUrl + CONSTANTS.liveCommunityUploadsPath,
+          headers: {},
+          data: {
+            files: files
+          }
+        }).then(function (response) {
+          $timeout(function () {
+            console.log(response);
+          });
+        }, function (response) {
+
+          /*if (response.status > 0) {
+           $scope.errorMsg = response.status + ': ' + response.data;
+           }* /
+
+          //TODO : Display message to User
+
+        }, function (evt) {
+          /*$scope.progress =
+           Math.min(100, parseInt(100.0 * evt.loaded / evt.total));* /
+          //Nothing to do ?
+        });
+
+        /*xhr(CONSTANTS.liveCommunityServiceUrl + CONSTANTS.liveCommunityUploadsPath, JSON.stringify(files), function(_fileName) {
+         var href = location.href.substr(0, location.href.lastIndexOf('/') + 1);
+         cameraPreview.src = href + 'uploads/' + _fileName;
+         cameraPreview.play();
+
+         var h2 = document.createElement('h2');
+         h2.innerHTML = '<a href="' + cameraPreview.src + '">' + cameraPreview.src + '</a>';
+         document.body.appendChild(h2);
+         });* /
+      }
+    };*/
+
+    /*function xhr(url, data, callback) {
       var request = new XMLHttpRequest();
       request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
@@ -122,7 +289,7 @@ angular.module('PulseTotemVideo')
       };
       request.open('POST', url);
       request.send(data);
-    }
+    }*/
 
     window.onbeforeunload = function() {
       startRecording.disabled = false;
